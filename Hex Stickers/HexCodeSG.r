aca49c8ba2573f9f98db1f1f20738acf78de3508
shohanghatak <shohanghatak@gmail.com>
2026-01-06 19:39:31 -0500

    Hex Sticker

---
+++ Hex Stickers/HexCodeSG.r
@@ -0,0 +1,52 @@
+library(hexSticker)
+library(ggplot2)
+library(ragg)
+
+## Set working directory or give full file path:
+my_image = "HexLogo.png"
+
+sticker_plot = sticker(
+  subplot  = my_image,
+ package  = "FSU \nBiostats",
+lineheight = 0.035,
+p_size = 100,
+p_x = 1, 
+ p_y = 1.65,
+s_x = 1, 
+s_y = 0.84, 
+s_width = 0.95,
+ p_color = "#FFFFFF",
+ h_color = "#27554b", h_fill = "#19417b",
+h_size  = 1.50, # THICK BORDER
+url     = "https://nishanmudalige.github.io/UTM_FSU_Biostats/",
+u_color = "#FFFFFF", 
+u_size = 20,
+white_around_sticker = FALSE,
+ filename = "RO_hex_sticker.png"
+)
+
+## Add padding around Hex sticker
+extra_padding = 0.05    # adjust this value as required
+
+
+## Add extra padding to sticker_plot
+sticker_plot = sticker_plot +
+  coord_fixed(clip = "off") +
+  scale_x_continuous(expand = expansion(mult = 0, add = extra_padding)) +
+  scale_y_continuous(expand = expansion(mult = 0, add = extra_padding)) +
+  theme(plot.margin = margin(6, 6, 6, 6, "mm"))  
+
+
+# Create a larger canvas
+canvas_scale = 1.12 # Adjust this value as required
+w_mm = 43.9 * canvas_scale # NOT recommended to adjust this value. Maintains aspect raio
+h_mm = 50.8 * canvas_scale # NOT recommended to adjust this value. Maintains aspect raio
+
+## Overwrite over 
+ggplot2::ggsave(
+  "R0_hex_sticker.png", 
+  sticker_plot,
+  bg = "transparent",
+  width = w_mm, height = h_mm, units = "mm",
+  dpi = 2500, limitsize = FALSE
+)
